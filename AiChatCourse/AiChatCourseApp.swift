5561116c5b8a610d20a274bdf758b0b91d1df2b6
Rogerio Cesar Gomes Pires <rogeriocpires@hotmail.com>
2025-02-18 11:04:55 -0300

    Initial Commit

---
+++ AiChatCourse/AiChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatCourseApp.swift
+//  AiChatCourse
+//
+//  Created by Rogerio Pires on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AiChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
